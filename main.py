e383dc0180c896ed5c6c0d882ece2f3135d831e9
ArtemG8 <120787936+ArtemG8@users.noreply.github.com>
2026-01-16 00:16:59 +0300

    init: старт бота инф накрутка

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.default import DefaultBotProperties
-from config import conf
-from keyboards import set_main_menu
 
 # Настройка логирования
 logger = logging.getLogger(__name__)
@@ -22,22 +20,20 @@ async def main():
     storage = MemoryStorage()
 
     # Инициализируем бота и диспетчера
-    bot = Bot(token=conf.BOT_TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
+    # bot = Bot(token=config.BOT_TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
     dp = Dispatcher(storage=storage)
 
 
-    # Установка команд меню
-    await set_main_menu(bot)
 
-    # Создаем таблицы в БД, если их нет
-    logger.info("Creating database tables if not exist...")
-    await create_db_and_tables()
-    logger.info("Database tables checked/created.")
-
-
-    # Пропускаем накопившиеся апдейты и запускаем polling
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    # # Создаем таблицы в БД, если их нет
+    # logger.info("Creating database tables if not exist...")
+    # await create_db_and_tables()
+    # logger.info("Database tables checked/created.")
+    #
+    #
+    # # Пропускаем накопившиеся апдейты и запускаем polling
+    # await bot.delete_webhook(drop_pending_updates=True)
+    # await dp.start_polling(bot)
 
 if __name__ == '__main__':
     try:
